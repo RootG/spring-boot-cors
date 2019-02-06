4b57e5de2becd5e54cd5f3430b77d415b7a80601
Görkem Mülayim <gorkemmulayim@icloud.com>
2019-02-07 01:03:34 +0300

    Added the test case.

---
+++ src/test/java/com/github/rootg/springbootcors/SpringBootCorsApplicationTest.java
@@ -0,0 +1,31 @@
+package com.github.rootg.springbootcors;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.options;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class SpringBootCorsApplicationTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testCors() throws Exception {
+        mockMvc.perform(options("/hello")
+                .header("Origin", "SpringBootCorsApplicationTest")
+                .header("Access-Control-Request-Method", "GET"))
+                .andExpect(status().isOk())
+                .andExpect(header().exists("Access-Control-Allow-Origin"))
+                .andExpect(header().exists("Access-Control-Allow-Credentials"));
+    }
+}
